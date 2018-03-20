9fd152a975735fdb176464159eddcad61a670d87
Kamigami55 <samkami@icloud.com>
2018-03-21 04:33:35 +0800

    Fix fclose problem & Unify coding style

--- pipeline.c
+++ pipeline.c
@@ -10,13 +10,13 @@
 
 long neon_iteration;
 
-float32_t mul(float *source,float *weight);
+float32_t mul(float *source, float *weight);
 void create_input(int size);
 
 static long diff_in_us(struct timespec t1, struct timespec t2)
 {
     struct timespec diff;
-    if (t2.tv_nsec-t1.tv_nsec < 0) {
+    if (t2.tv_nsec - t1.tv_nsec < 0) {
         diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
     } else {
@@ -30,32 +30,34 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 int main(int argc, char *argv[])
 {
 
-    struct timespec start,end;
+    struct timespec start, end;
     float source[SIZE] ;
     float weight[SIZE] ;
     char source_char[10];
     char weight_char[10];
     FILE *fptr;
     create_input(SIZE);
-    fptr = fopen(FILE_NAME,"r");
-    for(int i = 0; i<SIZE; i++) {
-        fscanf(fptr,"%s %s %f %f\n",source_char,weight_char,&source[i],&weight[i]);
+    fptr = fopen(FILE_NAME, "r");
+    for (int i = 0; i < SIZE; i++) {
+        fscanf(fptr, "%s %s %f %f\n", source_char, weight_char, &source[i], &weight[i]);
     }
-    clock_gettime(CLOCK_REALTIME,&start);
-    printf("output:  %f\n",mul(source,weight));
-    clock_gettime(CLOCK_REALTIME,&end);
-    printf("spend:  %ld us\n",diff_in_us(start,end));
+    fclose(fptr);
+    clock_gettime(CLOCK_REALTIME, &start);
+    printf("output:  %f\n", mul(source, weight));
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("spend:  %ld us\n", diff_in_us(start, end));
+
     return 0;
 }
 
-float32_t mul(float *source,float *weights)
+float32_t mul(float *source, float *weights)
 {
-    float32x4_t in1_128,in2_128,sum1,sum2,prod;
+    float32x4_t in1_128, in2_128, sum1, sum2, prod;
     float32_t result[4];
     float32_t output = 0.0;
     int i;
     prod = vmovq_n_f32(0.0f);
-    for (i=0; i<SIZE; i+=4) {
+    for (i = 0; i < SIZE; i += 4) {
         in1_128 = vld1q_f32(&source[i]);
         in2_128 = vld1q_f32(&weights[i]);
 #ifdef FLUSH
@@ -82,14 +84,14 @@ float32_t mul(float *source,float *weights)
 void create_input(int size)
 {
     FILE *fptr;
-    if( access( FILE_NAME, F_OK ) != -1 ) {
+    if (access(FILE_NAME, F_OK) != -1) {
         return;
     }
 
-    fptr = fopen(FILE_NAME,"w");
+    fptr = fopen(FILE_NAME, "w");
     srand(time(NULL));
-    for(int i = 0; i<size; i++) {
-        fprintf(fptr,"source weight %f %f\n",(float32_t)(rand()%4)+1,(float32_t)(rand()%4)+1);
+    for (int i = 0; i < size; i++) {
+        fprintf(fptr, "source weight %f %f\n", (float32_t)(rand() % 4) + 1, (float32_t)(rand() % 4) + 1);
     }
     fclose(fptr);
 }
